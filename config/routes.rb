cdcabf2e75ff20dadd7eaa663c76b5e22fffc5de
A-K796 <69107060+A-K796@users.noreply.github.com>
2020-09-17 19:43:05 +0900

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books
+  resources :books,only:[:create, :show, :edit, :update, :destroy, :index]
   
   resources :users,only:[:show, :edit, :update, :index]
   get 'home/about'
   root 'home#top'
 end
+
